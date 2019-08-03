ba55abeb4adb273af8d4fc430a9b3b8e4b842392
Andreas Holstenson <a@holstenson.se>
2019-08-03 08:56:54 +0200

    chore: Moving exports into sub-folders

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,8 @@ export * from './service';
 export * from './predicate';
 export * from './mapper';
 
-export * from './discovery/service-discovery';
-export * from './discovery/releaseable-service-discovery';
+export * from './discovery';
 
-export {
-	AbstractServiceDiscovery,
-	BasicServiceDiscovery,
-	LayeredServiceDiscovery,
-	MergedServiceDiscovery
-} from './discovery/internal';
-
-export * from './discovery/expiring-service-discovery';
-export * from './discovery/timed-service-discovery';
-export * from './discovery/manual-service-discovery';
-
-export * from './publishing/service-publisher';
-export * from './publishing/abstract-service-publisher';
+export * from './publishing';
 
 export * from './types';
