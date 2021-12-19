056d494ab28d7382d98a94f22b077404595c98a7
Andreas Holstenson <a@holstenson.se>
2021-12-19 10:25:31 +0100

    build: Run tests using ESM

--- jest.config.cjs
+++ jest.config.cjs
@@ -10,11 +10,6 @@ module.exports = {
 	},
 	"testEnvironment": "node",
 	"testRegex": "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
-	"moduleFileExtensions": [
-		"ts",
-		"tsx",
-		"js"
-	],
 	"coveragePathIgnorePatterns": [
 		"/node_modules/",
 		"/test/"
